c49d53e9473d5375e5598fbfeffe842a903417b8
Gajendar <gyrao72@gmail.com>
2021-06-20 21:45:12 +0530

    passport

--- models/user.js
+++ models/user.js
@@ -1,22 +1,15 @@
 const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
+const passportLocalMongoose=require('passport-local-mongoose');
 
-const userSchema=new Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
+const User=new Schema({
     admin:{
         type:Boolean,
         default:false
     }
 });
 
-var User=mongoose.model('User',userSchema);
 
-module.exports=User;
+User.plugin(passportLocalMongoose);
+
+module.exports=mongoose.model('User',User);
